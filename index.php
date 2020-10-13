fa1b0ef561b5df44254bf812a29da62e972eba73
Erik <erikhubers@users.noreply.github.com>
2020-10-14 01:43:59 +0200

    Update and rename dns.php to index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ else if (isset($_GET['dns']))
 {
         $request = base64_decode(str_replace(array('-', '_'), array('+', '/'), $_GET['dns']));
         header("Content-Type: application/dns-message");
-        $s = fsockopen("udp://127.0.0.1", 53, $errno, $errstr);
+        $s = fsockopen("udp://192.168.1.5", 53, $errno, $errstr);
         if ($s)
         {
                 fwrite($s, $request);
